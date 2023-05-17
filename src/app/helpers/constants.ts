2d2a41fbf61a55d487b1f5ce4ae7881eea58e801
jormarcus <jormarcus92@gmail.com>
2023-05-17 09:20:08 -0400

    Message api and openai call

---
+++ src/app/helpers/constants.ts
@@ -0,0 +1,6 @@
+export const chatbotPrompt = `
+You are a helpful customer support chatbot embedded on a website. You are able to answer questions about the website and its content.
+
+Refuse any answer that does not have to do with the app or its content.
+Provide short, concise answers.
+`
--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -23,7 +23,7 @@ const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ message: 'test' }),
+        body: JSON.stringify({ messages: [message] }),
       });
 
       return response.body;
